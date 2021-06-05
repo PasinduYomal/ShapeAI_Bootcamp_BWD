d51d0cd823fccee3701336f50ec07bc995f7c5e9
PasinduYomal <84906514+PasinduYomal@users.noreply.github.com>
2021-06-06 01:15:08 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        I am Pasindu Jayasinghe an undergraduate student at University of
+        Westminster. This was an amazing bootcamp taken up by Shaurya Sinha. We
+        covered everything from the scratch including JavaScript, React.js,
+        HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
